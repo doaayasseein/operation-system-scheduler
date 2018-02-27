d2d468fbdc227c6dc3bf7111ef547d6f1c7e2024
Doaa Mohamed <36889658+doaayasseein@users.noreply.github.com>
2018-02-27 18:02:47 +0200

    Add files via upload

---
+++ OSScheduler.java
@@ -0,0 +1,17 @@
+
+package os.scheduler;
+import java.util.Scanner;
+public class OSScheduler {
+    public static void main(String[] args) {
+                    Scanner scan = new Scanner(System.in);
+       
+                 //System.out.println("enter burst of every task");
+     java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new NewJFrame().setVisible(true);}
+                                                    });
+       
+               
+          
+    
+}}
